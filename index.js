08155054faa87bef3ea3485f8b5a7867246a1db9
martasvobo <62069655+martasvobo@users.noreply.github.com>
2022-11-03 14:20:54 +0200

    successful byte array index retrieval

---
+++ index.js
@@ -0,0 +1,17 @@
+import { wasmBrowserInstantiate } from "./hello-world.js";
+
+const runWasm = async () => {
+  const wasmModule = await wasmBrowserInstantiate("index.wasm");
+
+  const exports = wasmModule.instance.exports;
+
+  const memory = exports.memory;
+
+  const wasmByteMemoryArray = new Uint8Array(memory.buffer);
+
+  console.log(`0th index of array: ${wasmByteMemoryArray[0]}`);
+
+  wasmByteMemoryArray[1] = 511;
+  console.log(exports.readWasmMemoryAndReturnIndexOne());
+};
+runWasm();
